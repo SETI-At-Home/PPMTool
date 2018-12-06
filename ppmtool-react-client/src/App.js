3ad88470c860f3a8fbe0f479c3e8705e2f70bc96
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2018-12-06 18:09:17 +0100

    Client routing added, rest controller finished

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Dashboard from './components/dashboard';
 import Header from './components/Layout/Header';
+import AddProject from './components/Project/AddProject';
+import store from './store';
+import UpdateProject from './components/Project/UpdateProject';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Header />
+						<Route exact path="/dashboard" component={Dashboard} />
+						<Route exact path="/addProject" component={AddProject} />
+						<Route exact path="/updateProject/:id" component={UpdateProject} />
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
--- ppmtool/src/main/java/com/bozhinovski/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/bozhinovski/ppmtool/web/ProjectController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
